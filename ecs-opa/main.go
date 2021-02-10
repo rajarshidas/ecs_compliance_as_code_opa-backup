1dc575b2b38de2d70b48af2c5e07240462053a3d
rajarshidas <rajdasr@amazon.com>
2021-02-10 22:58:44 +0000

    configuring the function to access the mesages in the sns topic with sse w/kms

--- ecs-opa/main.go
+++ ecs-opa/main.go
@@ -274,13 +274,8 @@ func getAccountId() string {
 	result, err := svc.GetCallerIdentity(input)
 	if err != nil {
 		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
-			default:
-				fmt.Println(aerr.Error())
-			}
+			fmt.Println(aerr.Error())
 		} else {
-			// Print the error, cast err to awserr.Error to get the Code and
-			// Message from an error.
 			fmt.Println(err.Error())
 		}
 		log.Fatal("Error getting account id")
